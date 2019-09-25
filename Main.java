b3ab853860797dba5233f4be387b8dfe947d3675
Myx-x <mkamadeus.mka@gmail.com>
2019-09-26 00:55:27 +0700

    Fixed some IO issues

--- Main.java
+++ Main.java
@@ -7,44 +7,8 @@ public class Main
 {
     public static void main(String args[])
     {
-        /*
-        AugmentedMatrix aug = new AugmentedMatrix();
-
-        
-        aug.inputLinearEquation();
-        
-        aug.gaussJordanElimination();
-        aug.convertToSolutionInfinite();
-        System.out.println();
-        */
-
-        // aug.printAugmentedMatrix();
-        // System.out.println();
-        
-        // double[][] res = aug.infiniteSolutionMatrix();
-        // for(int i=0;i<aug.getLeftMatrix().colCount;i++)
-        // {
-        //     for(int j=0;j<aug.getLeftMatrix().colCount+1;j++) System.out.print(res[i][j] + " ");
-        //     System.out.println();
-        // }
-        // aug.backwardSubstitution();
-        // for(int i=0;i<aug.getLeftMatrix().colCount;i++)
-        // {
-        //     for(int j=0;j<aug.getLeftMatrix().colCount+1;j++) System.out.print(res[i][j] + " ");
-        //     System.out.println();
-        // }
-        // aug.convertToSolutionInfinite();
-        // Determinant d = new Determinant();
-        // d.mat.inputMatrix();
-        
-        // Matrix result = new Matrix();
-        
-        // result = d.inverseGaussJordan();
-        // result.printMatrix();
-
-        //Matrix MT = new Matrix();
-        //MT.textToMatrix();
-        AugmentedMatrix augMat = new AugmentedMatrix();
-        augMat.textToAug();
+       AugmentedMatrix aug = new AugmentedMatrix();
+       aug = aug.makeInterpolationMatrix();
+       aug.convertToInterpolation(aug);
     }
 }
